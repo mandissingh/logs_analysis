23bba8adfc6b7a8e8942156500913440d432cabc
Mandeep Singh <mandissingh1@gmail.com>
2017-05-21 20:07:47 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
+
 import psycopg2
+
 
 # Database connection function
 def connect(database_name="news"):
     try:
         dataname = 'news'
         db = psycopg2.connect('dbname={}'.format(dataname))
-        #Connecting to the databse
+        # Connecting to the databse
         cursor = db.cursor()
         return db, cursor
     except:
         print("Error connecting to database")
 
+
 # Error calculation function to show errors more than 1%
 def Errors():
     connection, cursor = connect()
-    #Fetching days with more than 1% errors
+    # Fetching days with more than 1% errors
     query = """SELECT view1.time, (fault::float * 100)/( total::float)
             AS no_of_error FROM view1, view2 WHERE view1.time = view2.time
             AND(fault::float * 100)/( total::float) >= 1.0;"""
@@ -26,15 +30,17 @@ def Errors():
     for i in range(0, len(result), 1):
         print str(result[i][0]) + "-> " + str(result[i][1]) + "%"
 
+
 # Function displaying popular authors
 def popular_authors():
     connection, cursor = connect()
-    #Query to fetch popular authors
+    # Query to fetch popular authors
     query = """ SELECT COUNT(articles.author),authors.name
             FROM log, articles, authors WHERE path LIKE '/article%'
             AND log.path = CONCAT('/article/',articles.slug) AND
             articles.author = authors.id
-            GROUP BY articles.author, authors.name ORDER BY count(path) DESC;"""
+            GROUP BY articles.author, authors.name
+            ORDER BY count(path) DESC;"""
     cursor.execute(query)
     result = cursor.fetchall()
     connection.close()
@@ -43,10 +49,11 @@ def popular_authors():
     for i in range(0, len(result), 1):
         print result[i][1] + "-> " + str(result[i][0])
 
+
 # Function displaying popular articles
 def popular_articles():
     connection, cursor = connect()
-    #Fetching popular articles
+    # Fetching popular articles
     query = """SELECT title, COUNT(title) AS number FROM log, articles
             WHERE path = CONCAT('/article/', articles.slug) GROUP BY
             title ORDER BY number DESC LIMIT 3;"""
